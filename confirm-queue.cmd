157b1ce8e3740ee57f3ffb5f143e74b2b77037d4
Paul Melekhov <gugglegum@gmail.com>
2024-03-06 00:39:51 +0400

    Added pause at the end

--- confirm-queue.cmd
+++ confirm-queue.cmd
@@ -1,2 +1,3 @@
 @echo off
-"%~dp0bin\php.exe" -c "%~dp0bin\php.ini" -n "%~dp0php\console.php" "confirm-queue"
+"%~dp0bin\php-win.exe" -c "%~dp0bin\php.ini" -n "%~dp0php\console.php" "confirm-queue"
+@pause
