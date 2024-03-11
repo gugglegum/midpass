42b653338b2ec9a002070e4441064f6b2e8c5646
Paul Melekhov <gugglegum@gmail.com>
2024-03-12 01:14:32 +0400

    Force all PHP notices and warnings to be PHP exceptions

--- php/init.php
+++ php/init.php
@@ -1,3 +1,12 @@
 <?php
 
 const PROJECT_ROOT_DIR = __DIR__;
+
+// Force all PHP notices and warnings to be PHP exceptions
+set_error_handler(
+/**
+ * @throws ErrorException
+ */
+    function(int $severity, string $message, string $file, int $line) {
+        throw new ErrorException($message, 0, $severity, $file, $line);
+    }, E_ALL);
